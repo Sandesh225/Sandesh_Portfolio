f44867f021cb76e585e77994ee5ca076d590bc24
Sandesh Subedi <subedi.sandesh888@gmail.com>
2025-12-24 21:13:25 +0545

    Fixed Next.congif.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    reactCompiler: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
+  /* config options here */
+  reactCompiler: true,
 };
 
 export default nextConfig;
